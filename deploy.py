ea381c5a62b675896ee450b6ad156ecb2c77dea5
Lekan Awodoye <olalekan.awodoye@gmail.com>
2022-08-21 21:23:10 +0000

    version2

--- deploy.py
+++ deploy.py
@@ -1,5 +1,7 @@
 #Deploying a python web application
-echo "Hello World"
+#echo "Hello World"
 #= shell scripts
-python('Hello World')
+#python('Hello World')
 #=python script
+
+print('Hello World')
